18411d54c34add4c4f7b7f82311dcad3841a7c98
Dan Avner <dan.avner@noirlab.edu>
2024-11-26 16:30:43 -0800

    GOATS-173: Add navbar to observation page.
    - Created new template tag to handle target context.

--- src/goats_tom/templatetags/__init__.py
+++ src/goats_tom/templatetags/__init__.py
@@ -1,6 +1,7 @@
 from .bootstrap4_overrides import bootstrap_pagination
 from .custom_filters import starts_with
 from .gemini import render_goa_query_form, render_launch_dragons
+from .target_navbar import render_target_navbar
 from .tom_overrides import goats_dataproduct_list_for_observation_saved
 
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     "render_launch_dragons",
     "goats_dataproduct_list_for_observation_saved",
     "bootstrap_pagination",
+    "render_target_navbar"
 ]
