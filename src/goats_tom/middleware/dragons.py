a3d6f3a120b68db5524076c8cfafc2cab08b1c3c
Dan Avner <dan.avner@noirlab.edu>
2025-03-05 16:49:00 -0800

    GOATS-653 GOATS-660: Develop middleware to cleanly shut down DRAGONS operations in worker threads.
    - Remove orphaned processes on GOATS shutdown.

---
+++ src/goats_tom/middleware/dragons.py
@@ -0,0 +1,48 @@
+__all__ = ["DRAGONSMiddleware"]
+
+from dramatiq import Broker, Worker
+from dramatiq.middleware import Middleware
+from gempy.eti_core.eti import ETISubprocess
+
+
+class DRAGONSMiddleware(Middleware):
+    """Middleware to ensure DRAGONS is properly cleaned up shut down."""
+
+    def before_worker_shutdown(self, broker: Broker, worker: Worker) -> None:
+        """Ensures `DRAGONS.ETISubprocess` is terminated when a Dramatiq worker shuts
+        down.
+
+        Parameters
+        ----------
+        broker : `Broker`
+            The broker managing message processing.
+        worker : `Worker`
+            The worker instance that is shutting down.
+        """
+        print("DRAGONSMiddleware: Terminating DRAGONS subprocesses on worker shutdown.")
+
+        try:
+            eti_subprocess = ETISubprocess()
+            # Send quit message to loop.
+            # Leaving in hoping DRAGONS merges in requested changes.
+            # eti_subprocess.inQueue.put(None)
+            eti_subprocess.process.join(timeout=2)
+
+            # If still alive, force-terminate.
+            if eti_subprocess.process.is_alive():
+                eti_subprocess.process.terminate()
+                eti_subprocess.process.join()
+
+            # Free up resources.
+            eti_subprocess.process.close()
+
+            # Ensure queues are properly closed after the process is dead.
+            try:
+                eti_subprocess.inQueue.close()
+                eti_subprocess.inQueue.join_thread()
+                eti_subprocess.outQueue.close()
+                eti_subprocess.outQueue.join_thread()
+            except Exception:
+                pass
+        except Exception as e:
+            print(f"Error during DRAGONS shutdown: {e}")
